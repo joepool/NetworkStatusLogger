c444a1c684e95200fff1c282b42e28773810b5ab
joepool <65784403+joepool@users.noreply.github.com>
2020-08-23 19:46:51 +0100

    Add Raspberry Pi Service

--- NetworkStatusLogger.sh
+++ NetworkStatusLogger.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 dir=("/home/pi/networkstatuslog.txt")
-down=(": Netowork is Down")
+down=("- Netowork is Down")
 while :
 do
 	now=$(date)
-	echo $now
 	ping -c 1 8.8.8.8 > /dev/null 2>&1
 	ping=$?
 	if [ "$ping" -eq "2" ]
@@ -12,8 +11,6 @@ do
 		output="${now} ${down}"
 		echo $output >> $dir
 		echo Network Down
-	else
-		echo Network Up
 	fi
 	sleep 20
 done
