d99783ef68fd2441955a488f7797f3e27d8748c5
joepool <65784403+joepool@users.noreply.github.com>
2020-09-16 16:59:37 +0100

    Use HTTP request instead of ping

--- NetworkStatusLogger.sh
+++ NetworkStatusLogger.sh
@@ -5,7 +5,7 @@ do
 	now=$(date)
 	case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
   	[23]):;;
- 	*) echo "The network is down or very slow";output="${now} - Network is Down";echo "$output" >> "$dir";;
+ 	*) echo "Network is Down";output="${now} - Network is Down";echo "$output" >> "$dir";;
 	esac
 	sleep 30
 done
