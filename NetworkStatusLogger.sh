4bf089300a884e0c18b13e2003c2a00b4e9805c8
joepool <65784403+joepool@users.noreply.github.com>
2020-09-17 16:32:25 +0100

    Increase HTTP timeout to prevent false positives

--- NetworkStatusLogger.sh
+++ NetworkStatusLogger.sh
@@ -3,7 +3,7 @@ dir=("/home/pi/networkstatuslog.txt")
 while :
 do
 	now=$(date)
-	case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
+	case "$(curl -s --max-time 5 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
   	[23]):;;
  	*) echo "Network is Down";output="${now} - Network is Down";echo "$output" >> "$dir";;
 	esac
