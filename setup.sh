bae3210a5655831cad08dd096c13b4555361472a
joepool <65784403+joepool@users.noreply.github.com>
2020-09-17 23:32:26 +0100

    Fix setup files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cp /home/pi/NetworkStatusLogger/NetworkStatusLogger.service /etc/systemd/system/NetworkStatusLogger.service
 systemctl enable NetworkStatusLogger.service
+echo Your Pi will now reboot...
+sleep 5
 reboot
--- setup_inc_web.sh
+++ setup_inc_web.sh
@@ -4,6 +4,11 @@ apt install php libapache2-mod-php -y
 chown pi:/var/www/html/index.html
 rm /var/www/html/index.html
 cp /home/pi/NetworkStatusLogger/index.php /var/www/html.index.php
+cp /home/pi/NetworkStatusLogger/clear.php /var/www/html.clear.php
+cp /home/pi/NetworkStatusLogger/style.css /var/www/html.style.css
+cp /home/pi/NetworkStatusLogger/scripts.js /var/www/html.scripts.js
 cp /home/pi/NetworkStatusLogger/NetworkStatusLogger.service /etc/systemd/system/NetworkStatusLogger.service
 systemctl enable NetworkStatusLogger.service
+echo Your Pi will now reboot...
+sleep 5
 reboot
