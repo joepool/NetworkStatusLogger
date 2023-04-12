2d31cf94ca6297967423edb20823d5d29519d9bb
joepool <65784403+joepool@users.noreply.github.com>
2023-04-12 03:54:30 +0100

    Update setup_inc_web.sh

--- setup_inc_web.sh
+++ setup_inc_web.sh
@@ -3,10 +3,10 @@ apt install apache2 -y
 apt install php libapache2-mod-php -y
 chown pi:/var/www/html/index.html
 rm /var/www/html/index.html
-cp /home/pi/NetworkStatusLogger/index.php /var/www/html.index.php
-cp /home/pi/NetworkStatusLogger/clear.php /var/www/html.clear.php
-cp /home/pi/NetworkStatusLogger/style.css /var/www/html.style.css
-cp /home/pi/NetworkStatusLogger/scripts.js /var/www/html.scripts.js
+cp /home/pi/NetworkStatusLogger/index.php /var/www/html/index.php
+cp /home/pi/NetworkStatusLogger/clear.php /var/www/html/clear.php
+cp /home/pi/NetworkStatusLogger/style.css /var/www/html/style.css
+cp /home/pi/NetworkStatusLogger/scripts.js /var/www/html/scripts.js
 cp /home/pi/NetworkStatusLogger/NetworkStatusLogger.service /etc/systemd/system/NetworkStatusLogger.service
 systemctl enable NetworkStatusLogger.service
 echo Your Pi will now reboot...
